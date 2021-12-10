7e341bbadbe964ed234abe4730ae1a46673e94e5
brahim <brahimgb1team@gmail.com>
2021-12-10 21:37:01 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,65 +1,36 @@
 import './style.css';
 
-const addBtn = document.querySelector('#add-btn');
-const todoList = document.querySelector('.todo-list');
-const inputField = document.querySelector('#input-field');
-const clearAll = document.querySelector('.clearAll');
-const container = document.querySelector('.container');
-const inputDiv = document.querySelector('.input-div');
-
-const todoListArray = [
+const myList = [
   {
-    description: 'none',
+    description: 'take your breakfast',
     completed: false,
     index: 0,
   },
+  {
+    description: 'study your books',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'make the dinner',
+    completed: false,
+    index: 2,
+  },
 ];
 
-const addNew = () => {
-  if (inputField.value === '') {
-    const warn = document.createElement('h2');
-    warn.classList.add('warn');
-    warn.innerHTML = 'Please type something';
-    container.insertBefore(warn, inputDiv);
-    setTimeout(() => warn.remove(), 1000);
-  } else {
-    const div = document.createElement('div');
-    div.classList.add('div-active');
-    div.innerHTML = inputField.value;
-    todoList.insertBefore(div, clearAll);
-    inputField.value = '';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    div.appendChild(checkbox);
-    checkbox.classList.add('checkbox');
-    const checked = () => {
-      if (checkbox.checked === false) {
-        checkbox.parentElement.classList.remove('line-through');
-      } else {
-        checkbox.parentElement.classList.add('line-through');
-      }
-    };
-    checkbox.addEventListener('click', checked);
-    clearAll.classList.remove('dis-none');
-    const todo = {
-      description: inputField.value,
-      completed: checkbox.checked,
-      index: 0,
-    };
-    todoListArray.push(todo);
-  }
-};
-const removeItems = () => {
-  const toRemove = document.querySelectorAll('.line-through');
-  toRemove.forEach((r) => {
-    r.remove();
+const list = document.getElementById('licontainer');
+
+// add items to dom
+const addNew = (array, element) => {
+  array.forEach((item) => {
+    const listItems = document.createElement('li');
+    listItems.classList.add('liststyle');
+    element.appendChild(listItems);
+    listItems.innerHTML = `
+    <input type="checkbox" class = "check" id="${item.index}">
+    <label for="${item.index}">${item.description}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <i class="fas fa-ellipsis-v"></i>
+    `;
   });
 };
-
-addBtn.addEventListener('click', addNew);
-clearAll.addEventListener('click', removeItems);
-inputField.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    addNew();
-  }
-});
+addNew(myList, list);
